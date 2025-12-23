653346ce69eb4a579e282dd98cc9a36912f69dc8
dasi <1740929297@qq.com>
2025-12-23 16:48:13 +0800

    【第 3 节】调用Ollama模型

---
+++ ai-rag-knowledge/ai-rag-knowledge-app/src/main/java/com/dasi/config/RedisClientConfig.java
@@ -0,0 +1,44 @@
+package com.dasi.config;
+
+import com.dasi.properties.RedisClientProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisClientConfig {
+
+    @Bean
+    public RedissonClient redissonClient(RedisClientProperties properties) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        JsonJacksonCodec codec = new JsonJacksonCodec(mapper);
+
+        Config config = new Config();
+        config.setCodec(codec);
+        String address = "redis://" + properties.getHost() + ":" + properties.getPort();
+        config.useSingleServer()
+                .setAddress(address)
+                .setPassword(properties.getPassword())
+                .setDatabase(properties.getDatabase())
+                .setConnectionPoolSize(properties.getPoolSize())
+                .setConnectionMinimumIdleSize(properties.getMinIdleSize())
+                .setIdleConnectionTimeout(properties.getIdleTimeout())
+                .setConnectTimeout(properties.getConnectTimeout())
+                .setRetryAttempts(properties.getRetryAttempts())
+                .setRetryInterval(properties.getRetryInterval())
+                .setPingConnectionInterval(properties.getPingInterval())
+                .setKeepAlive(properties.isKeepAlive())
+        ;
+
+        return Redisson.create(config);
+    }
+
+}
