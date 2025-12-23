5ff07d3bb3afa773eb03f275aeda46cb1df3e761
dasi <1740929297@qq.com>
2025-12-23 16:48:13 +0800

    【第 3 节】调用Ollama模型

--- ai-rag-knowledge/docs/api/curl.sh
+++
@@ -1,7 +0,0 @@
-curl http://localhost:11434/api/generate \
-  -H "Content-Type: application/json" \
-  -d '{
-        "model": "deepseek-r1:1.5b",
-        "prompt": "2*3=？",
-        "stream": false
-      }'
